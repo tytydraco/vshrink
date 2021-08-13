31db5429b2dd78a1ca623280f000651cb7814aba
Tyler <tylernij@gmail.com>
2021-08-12 20:00:27 -0700

    Re-encode to mkv

--- vshrink.sh
+++ vshrink.sh
@@ -16,8 +16,8 @@ shrink() {
 	video="$1"
 
 	filename=$(basename -- "$video")
-	extension="${video##*.}"
-	filename="${video%.*}"
+	extension="${filename##*.}"
+	video_no_ext="${video%.*}"
 	
 	metadata="$(ffprobe -v error -select_streams v:0 -show_entries stream=height,r_frame_rate -of csv=p=0 "$video")"
 	width="$(echo "$metadata" | awk -F , '{print $1}')"
@@ -25,14 +25,14 @@ shrink() {
 
 	[[ "$width" -gt 720 ]] && args+=("-vf" "scale='-2:720'")
 	[[ "$rate" -gt 24 ]] && args+=("-vsync" "vfr" "-r" "24")
-
+	[[ "${#args[@]}" -eq 0 && "$extension" != "mkv" ]] && args+=("-c:av" "copy")
 	[[ "${#args[@]}" -eq 0 ]] && return
 
 	echo "[SHRINKING] $filename"
 	if ffmpeg -y -i "$video" -f matroska "${args[@]}" tmp
 	then
 		rm -f "$video"
-		mv tmp "$filename.mkv"	
+		mv tmp "$video_no_ext.mkv"	
 	else
 		echo "[ERROR]"
 		rm tmp
